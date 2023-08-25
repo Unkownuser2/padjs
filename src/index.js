a6466f4ce166d92d9596aa0172dcde0351460599
Mathias Fantoni <MathiasFantoni@Gmail.com>
2023-08-25 18:01:02 +0200

    Clean up (Prettier and fixups)

--- src/index.js
+++ src/index.js
@@ -1,72 +1,75 @@
-require('dotenv').config();
-const { Client, IntentsBitField, ChannelType } = require('discord.js');
-const pre =('!p ')
+import { config } from 'dotenv';
+import { Client, IntentsBitField, ChannelType } from 'discord.js';
+
+config();
+
+const pre = '!p ';
 
 const client = new Client({
-    intents: [
-        1
-        // IntentsBitField.Flags.Guilds,
-        // IntentsBitField.Flags.GuildMembers,
-        // IntentsBitField.Flags.GuildMessages,
-        // IntentsBitField.Flags.MessageContent,
-        
-    ]
+  intents: [
+    1,
+    // IntentsBitField.Flags.Guilds,
+    // IntentsBitField.Flags.GuildMembers,
+    // IntentsBitField.Flags.GuildMessages,
+    // IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.on('ready', (c) =>{
-    client.user.setActivity({
-        name: 'Pengu'
-    })
-    console.log('bot on bro')
+client.on('ready', (c) => {
+  client.user.setActivity({
+    name: 'Pengu',
+  });
+  console.log('bot on bro');
 });
 
-client.on('messageCreate', msg => {
-    if (msg.author.bot){
-        return;
-    }
-    if(msg.content === pre+'commands'){
-        msg.reply('Hello comrade! I am PravdaAgendaBot, made by my glorius leader ``pengu211`` and great contributer ``mattphantastic``\n# Commands\n')
-    }
-})
+client.on('messageCreate', (msg) => {
+  if (msg.author.bot) {
+    return;
+  }
+  if (msg.content === pre + 'commands') {
+    msg.reply(
+      'Hello comrade! I am PravdaAgendaBot, made by my glorius leader ``pengu211`` and great contributer ``mattphantastic``\n# Commands\n'
+    );
+  }
+});
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === 'create-agenda') {
-        const user = interaction.user;
-        const Title = interaction.options.getString('title');
-        const Agenda = interaction.options.getString('agenda');
-        const InviteUser = interaction.options.getUser('invite');
-        const Invite = InviteUser ? InviteUser.toString() : null;
-        const Timestamp = interaction.options.getString('timestamp');
+  if (interaction.commandName === 'create-agenda') {
+    const user = interaction.user;
+    const Title = interaction.options.getString('title');
+    const Agenda = interaction.options.getString('agenda');
+    const InviteUser = interaction.options.getUser('invite');
+    const Invite = InviteUser ? InviteUser.toString() : null;
+    const Timestamp = interaction.options.getString('timestamp');
 
-        const threadName = Title.replace(/\s+/g, '-');
-        const finalResultMessage = `### On ${Timestamp} \n# Meeting: ${Title}\n${Agenda}`;
-        console.log(finalResultMessage);
+    const threadName = Title.replace(/\s+/g, '-');
+    const finalResultMessage = `### On ${Timestamp} \n# Meeting: ${Title}\n${Agenda}`;
+    console.log(finalResultMessage);
 
-        await interaction.reply(finalResultMessage);
+    await interaction.reply(finalResultMessage);
 
-        const thread = await interaction.channel.threads.create({
-            name: threadName,
-            autoArchiveDuration: 60,
-            type: 11
-        });
+    const thread = await interaction.channel.threads.create({
+      name: threadName,
+      autoArchiveDuration: 60,
+      type: 11,
+    });
 
-        const threadChannel = await thread.join();
+    const threadChannel = await thread.join();
 
-        // Mention the author of the interaction in the new thread
-        if (Invite != null) {
-            await threadChannel.send(`${finalResultMessage}\n${Invite} ${user}`);
-        } else {
-            await threadChannel.send(`${finalResultMessage}\n${user}`);
-        }
+    // Mention the author of the interaction in the new thread
+    if (Invite != null) {
+      await threadChannel.send(`${finalResultMessage}\n${Invite} ${user}`);
+    } else {
+      await threadChannel.send(`${finalResultMessage}\n${user}`);
     }
-    if (interaction.commandName === 'about'){
-        interaction.reply('Hello comrade! I am Secretary, hired by the glorius leader ``pengu211`` and great contributer ``mattphantastic``\n# Commands\n```/create-agenda {Title} {Agenda} {Invite} {Timestamp}```')
-    }
+  }
+  if (interaction.commandName === 'about') {
+    interaction.reply(
+      'Hello comrade! I am Secretary, hired by the glorius leader ``pengu211`` and great contributer ``mattphantastic``\n# Commands\n```/create-agenda {Title} {Agenda} {Invite} {Timestamp}```'
+    );
+  }
 });
-
-
-
 
 // node src/index.js
 client.login(process.env.token);
--- src/regist-commands.js
+++ src/regist-commands.js
@@ -1,61 +1,58 @@
-require('dotenv').config();
-const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+import { config } from 'dotenv';
+import { REST, Routes, ApplicationCommandOptionType } from 'discord.js';
+
+config();
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const commands = [
-    {
-        name: 'about',
-        description: 'about',
-    },
-    
-    
-    {
-        name: 'create-agenda',
-        description: 'Creates an Agenda',
-        options: [
-            {
-                name: 'title',
-                description: 'The title of the Agenda',  
-                type: ApplicationCommandOptionType.String,
-                required: true,
-            },
-            
-            {
-                name: 'agenda',
-                description: 'Explain the Agenda.',  
-                type: ApplicationCommandOptionType.String,
-                required: true,
-            },
-            
-            {
-                name: 'invite',
-                description: 'Who do you want to invite to the agenda? (Users Only!)',  
-                type: ApplicationCommandOptionType.User,
-                required: true,
-            },
-            {
-                name: 'timestamp',
-                description: 'The timestamp you want to set. eg: <t:1691674200:F>',  
-                type: ApplicationCommandOptionType.String,
-                required: true,
-            },
-            
-        ]
-    }
+  {
+    name: 'about',
+    description: 'about',
+  },
+  {
+    name: 'create-agenda',
+    description: 'Creates an Agenda',
+    options: [
+      {
+        name: 'title',
+        description: 'The title of the Agenda',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+      {
+        name: 'agenda',
+        description: 'Explain the Agenda.',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+      {
+        name: 'invite',
+        description: 'Who do you want to invite to the agenda? (Users Only!)',
+        type: ApplicationCommandOptionType.User,
+        required: true,
+      },
+      {
+        name: 'timestamp',
+        description: 'The timestamp you want to set. eg: <t:1691674200:F>',
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
 ];
 
-
-const rest = new REST().setToken(process.env.token);
+const rest = new REST().setToken(TOKEN);
 
 (async () => {
-    try {
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.client_id, process.env.guild_id),
-            { body: commands },
-        );
-        console.log('Successfully registered');
-    } catch (error) {
-        console.error('Error:', error);
-    }
+  try {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+    console.log('Successfully registered');
+  } catch (error) {
+    console.error('Error:', error);
+  }
 })();
 
 //node src/regist-commands.js
