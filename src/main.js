ef1bdaa0585dd8351137f336ac38edfaf07844e6
Mathias Fantoni <MathiasFantoni@Gmail.com>
2023-08-26 14:02:33 +0200

    Register (/) commands and log in

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import { config } from 'dotenv';
+import { Client, IntentsBitField, SlashCommandBuilder, REST, Routes } from 'discord.js';
+
+config();
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageTyping,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildIntegrations,
+    IntentsBitField.Flags.GuildScheduledEvents,
+    IntentsBitField.Flags.GuildModeration,
+  ],
+});
+
+// prettier-ignore
+const commands = [
+  new SlashCommandBuilder().setName('meet').setDescription('Announce an upcoming meeting'),
+];
+
+const rest = new REST().setToken(TOKEN);
+
+(async function main() {
+  try {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    console.log('Registered application (/) commands');
+
+    await client.login(TOKEN);
+    console.log('Logged in the client');
+  } catch (error) {
+    console.error('An unexpected error occured, quitting...');
+    console.error('________________________________________');
+    console.error(error);
+  }
+})();
+
+// node src/main.js
